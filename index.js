659c8b0ac5617f260d2f93efad66cc8444b3272e
liuyang.0826 <1161906592@qq.com>
2022-05-09 16:48:49 +0800

    feat: remove at-rule

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const selectorPattern = "^([a-z][a-z0-9]*)(-{1,2}[a-z0-9]+)*(_{2}[a-z0-9]+)*$"
 module.exports = {
   extends: ["stylelint-config-standard-scss", "stylelint-config-recess-order"],
   rules: {
-    "at-rule-no-unknown": [
-      true,
-      {
-        ignoreAtRules: ["mixin", "extend", "content", "include"]
-      }
-    ],
     "selector-pseudo-class-no-unknown": [
       true, 
       {
